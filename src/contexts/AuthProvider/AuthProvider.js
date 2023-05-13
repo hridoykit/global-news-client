b0fbf00dee33688eb362ff7809148928cca6726a
hridoykit <hridoy.tsa@gmail.com>
2023-05-13 22:30:55 +0600

    store the user info

--- src/contexts/AuthProvider/AuthProvider.js
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -1,17 +1,23 @@
 
-import { signInWithPopup } from 'firebase/auth';
-import React, { createContext } from 'react';
+import { onAuthStateChanged, signInWithPopup } from 'firebase/auth';
+import React, { createContext, useEffect, useState } from 'react';
 import auth from '../../firebase/firebase.init';
 
 export const AuthContext = createContext();
 
 const AuthProvider = ({children}) => {
-    const user = {name: "Hridoy"};
+    // const user = {name: "Hridoy"};
+    const [user, setUser] = useState(null);
 
     const loginWithGoogle = (provider) =>{
         return signInWithPopup(auth, provider);
     };
     
+    useEffect(() =>{
+        onAuthStateChanged(auth, (currentUser) =>{
+            setUser(currentUser);
+        })
+    }, []);
 
     //const authInfo = {user, googleSignIn};
     return (
